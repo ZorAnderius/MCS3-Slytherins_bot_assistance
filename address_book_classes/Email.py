147e6763e2906b28b7df99c8c1ed1d588ffcfb2c
AntonRoot <144062316+AntonRoot@users.noreply.github.com>
2023-11-01 18:42:09 +0100

    Update Email.py
    create class: email with validation

--- address_book_classes/Email.py
+++ address_book_classes/Email.py
@@ -1,2 +1,18 @@
+import re
+
 class Email:
-    pass
+    def __init__(self, email):
+        if self.validate_email(email):
+            self.email = email
+        else:
+            raise ValueError("Invalid email address")
+
+    def validate_email(self, email):
+        email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        return re.match(email_pattern, email) is not None
+
+try:
+    my_email = Email("example@example.com")
+    print(f"Email address: {my_email.email}")
+except ValueError as e:
+    print(e)
