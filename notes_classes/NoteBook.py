b2e3f2dd5723003e78b087c8f17636bb599b2015
ZorAnderius <gabriel.zorander@gmail.com>
2023-11-02 00:20:30 +0000

    delete testing code

--- notes_classes/NoteBook.py
+++ notes_classes/NoteBook.py
@@ -34,28 +34,3 @@ class NoteBook(UserDict):
         for note in self.data.values():
             string += str(note) + "\n"
         return string[:-1:]
-
-
-def show_all(book):
-    general_str = ""
-    for val, record in book.items():
-        general_str += str(record)
-    if general_str == "":
-        return "Book is empty"
-    return general_str[:-1:]
-
-
-if __name__ == "__main__":
-    notebook = NoteBook()
-    note = Note()
-    note.add_title("")
-    note.add_body("It's my first program")
-    notebook.add_note(note)
-    # print(show_all(notebook))
-    # time.sleep(30)
-    note2 = Note()
-    note2.add_title("my first program")
-    note2.add_body("It's my first program")
-    # print(note2.created_at == note.created_at)
-    notebook.add_note(note2)
-    print(show_all(notebook))
