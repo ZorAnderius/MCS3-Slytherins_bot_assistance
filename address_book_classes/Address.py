0e34443f2f7f074178b6935d8f986f5bafab3c65
kettycollins <k.pankieieva@gmail.com>
2023-11-02 17:12:10 +0100

    added class Address with address verification
    address shouldn`t be empty and should contain at least one letter

--- address_book_classes/Address.py
+++ address_book_classes/Address.py
@@ -1,2 +1,29 @@
 class Address:
-    pass
+    def __init__(self, text):
+        if not text:
+            raise ValueError("Будь ласка, уведіть текст у текстовому форматі.")
+        if not any(char.isalpha() for char in text):
+            raise ValueError("Текст повинен містити принаймні одину літеру.")
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+# приклади використання
+try:
+    address1 = Address("Кучеренка 8, Полтава")
+    print(address1)
+except ValueError as e:
+    print(e)
+
+try:
+    address2 = Address("36009")
+    print(address2)
+except ValueError as e:
+    print(e)
+
+try:
+    address3 = Address("")
+    print(address3)
+except ValueError as e:
+    print(e)
