354002a5e6364f8b38665ded1597116c0546ddf1
kettycollins <k.pankieieva@gmail.com>
2023-11-02 22:00:33 +0100

    class Address

--- address_book_classes/Address.py
+++ address_book_classes/Address.py
@@ -1,29 +1,10 @@
 class Address:
     def __init__(self, text):
         if not text:
-            raise ValueError("Будь ласка, уведіть текст у текстовому форматі.")
+            raise ValueError("Please put text in the text format.")
         if not any(char.isalpha() for char in text):
-            raise ValueError("Текст повинен містити принаймні одину літеру.")
+            raise ValueError("Text must contain at least one alphabet character.")
         self.text = text
 
     def __str__(self):
         return self.text
-
-# приклади використання
-try:
-    address1 = Address("Кучеренка 8, Полтава")
-    print(address1)
-except ValueError as e:
-    print(e)
-
-try:
-    address2 = Address("36009")
-    print(address2)
-except ValueError as e:
-    print(e)
-
-try:
-    address3 = Address("")
-    print(address3)
-except ValueError as e:
-    print(e)
