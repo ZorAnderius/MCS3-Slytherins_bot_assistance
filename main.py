ae2ec47493000a953bf645d9869c0fd20b279c3f
kettycollins <k.pankieieva@gmail.com>
2023-11-05 13:17:15 +0100

    autocomplete

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from colorama import Fore
 from pathlib import Path
 import os.path
 from rich.console import Console
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
 
 from address_book_classes.AddressBook import AddressBook
 from notes_classes.NoteBook import NoteBook
@@ -13,6 +15,8 @@ notebook_path = Path("note_book.json")
 
 
 def main():
+    book_path = Path("address_book.json")
+    notebook_path = Path("note_book.json")
     book = AddressBook()
     notebook = NoteBook()
     console = Console()
@@ -24,83 +28,99 @@ def main():
         new_notebook = notebook.read_from_file(notebook_path )
         if new_notebook and type(new_notebook) is dict:
             notebook = notebook.add_book(new_notebook)
+    
     while True:
-        user_input = input(Fore.CYAN + "Enter a command: ")
+        user_input = prompt(Fore.CYAN + "Enter a command: ", completer=command_completer())
         if user_input:
             command, *args = parse_input(user_input)
-            if command in ["close", "exit"]:
-                print(Fore.BLUE + "Good bye!")
-                break
-            elif command == "hello":
-                print(Fore.BLUE + "How can I help you?")
-            elif command == "add-contact":
-                print(added_contact(args, book))
-            elif command == "change-contact":
-                print(change_contact(args, book))
-            elif command == "add-email":
-                print(add_email(args, book))
-            elif command == "change-email":
-                print(change_email(args, book))
-            elif command == "add-address":
-                print(add_address(args, book))
-            elif command == "change-address":
-                print(change_address(args, book))
-            elif command == "phone":
-                print(find_phone(args, book)[:-1:])
-            elif command == "all-contacts":
-                console.print(show_all(book))
-            elif command == 'delete-record':
-                print (delete_record(args, book))
-            elif command == "search-by-name":
-                result = search_by_name(args, book)
-                if result is not None:
-                    console.print(result)
-            elif command == "search-by-phone":
-                result = search_by_phone(args, book)
-                if result is not None:
-                    console.print(result)
-            elif command == "search-by-email":
-                result = search_by_email(args, book)
-                if result is not None:
-                    console.print(result)
-            elif command == "remove-phone":
-                remove_phone(args, book)
-            elif command == "add-birthday":
-                print(add_birthday(args, book))
-            elif command == "show-birthday":
-                print(show_birthday(args, book))
-            elif command == "birthdays":
-                print(show_all_birthdays(args, book))
-            elif command == "add-note":
-                print(add_note(args, notebook))
-            elif command == "add-tag":
-                print(add_tag(args, notebook))
-            elif command == "change-title":
-                print(change_note_title(args, notebook))
-            elif command == "change-body":
-                print(change_note_body(args, notebook))
-            elif command == "change-tag":
-                print(change_note_tag(args, notebook))
-            elif command == "delete-notes":
-                print(delete_notes(args, notebook))
-            elif command == "remove-note":
-                print(remove_note(args, notebook))
-            elif command == "remove-body":
-                print(remove_note_body(args, notebook))
-            elif command == "remove-tag":
-                print(remove_note_tag(args, notebook))
-            elif command == "search-tag":
-                console.print(search_by_tag(args, notebook))
-            elif command == "search-author":
-                console.print(search_by_author(args, notebook))
-            elif command == "search-title":
-                console.print(search_by_title(args, notebook))
-            elif command == "sort-notes":
-                console.print(sort_notes(notebook))
-            elif command == "all-notes":
-                console.print(show_all(notebook))
-            else:
-                print(Fore.YELLOW + "Invalid command")
+            handle_command(command, args, book, notebook, console, book_path, notebook_path)
+
+def command_completer():
+    book_commands = ['add-contact', 'change-contact', 'add-email', 'change-email', 'add-address', 'change-address',
+                     'phone', 'all-contacts', 'delete-record', 'search-by-name', 'search-by-phone', 'search-by-email',
+                     'remove-phone', 'add-birthday', 'show-birthday', 'birthdays']
+    notebook_commands = ['add-note', 'add-tag', 'change-title', 'change-body', 'change-tag', 'remove-note',
+                        'remove-body', 'remove-tag', 'all-notes']
+    commands = book_commands + notebook_commands
+    return WordCompleter(commands)
+
+
+def handle_command(command, args, book, notebook, console, book_path, notebook_path):
+    if command == "close" or command == "exit":
+        print(Fore.BLUE + "Good bye!")
+        book.save_to_file(book_path)
+        notebook.save_to_file(notebook_path)
+        sys.exit(0)
+    elif command == "hello":
+        print(Fore.BLUE + "How can I help you?")
+    elif command == "add-contact":
+        print(added_contact(args, book))
+    elif command == "change-contact":
+        print(change_contact(args, book))
+    elif command == "add-email":
+        print(add_email(args, book))
+    elif command == "change-email":
+        print(change_email(args, book))
+    elif command == "add-address":
+        print(add_address(args, book))
+    elif command == "change-address":
+        print(change_address(args, book))
+    elif command == "phone":
+        print(find_phone(args, book)[:-1:])
+    elif command == "all-contacts":
+        console.print(show_all(book))
+    elif command == 'delete-record':
+        print (delete_record(args, book))
+    elif command == "search-by-name":
+        result = search_by_name(args, book)
+        if result is not None:
+            console.print(result)
+    elif command == "search-by-phone":
+        result = search_by_phone(args, book)
+        if result is not None:
+            console.print(result)
+    elif command == "search-by-email":
+        result = search_by_email(args, book)
+        if result is not None:
+            console.print(result)
+    elif command == "remove-phone":
+        remove_phone(args, book)
+    elif command == "add-birthday":
+        print(add_birthday(args, book))
+    elif command == "show-birthday":
+        print(show_birthday(args, book))
+    elif command == "birthdays":
+        print(show_all_birthdays(args, book))
+    elif command == "add-note":
+        print(add_note(args, notebook))
+    elif command == "add-tag":
+        print(add_tag(args, notebook))
+    elif command == "change-title":
+        print(change_note_title(args, notebook))
+    elif command == "change-body":
+        print(change_note_body(args, notebook))
+    elif command == "change-tag":
+        print(change_note_tag(args, notebook))
+    elif command == "delete-notes":
+        print(delete_notes(args, notebook))
+    elif command == "remove-note":
+        print(remove_note(args, notebook))
+    elif command == "remove-body":
+        print(remove_note_body(args, notebook))
+    elif command == "remove-tag":
+        print(remove_note_tag(args, notebook))
+    elif command == "search-tag":
+        console.print(search_by_tag(args, notebook))
+    elif command == "search-author":
+        console.print(search_by_author(args, notebook))
+    elif command == "search-title":
+        console.print(search_by_title(args, notebook))
+    elif command == "sort-notes":
+        console.print(sort_notes(notebook))
+    elif command == "all-notes":
+        console.print(show_all(notebook))
+    else:
+        print(Fore.YELLOW + "Invalid command")
 
     book.save_to_file(book_path)
     notebook.save_to_file(notebook_path)
